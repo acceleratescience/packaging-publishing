dfa5dae99a8e646892df67e96f779a9a352b6c97
ZixuanHuang-Joy <joyhuanghzx@outlook.com>
2024-11-18 23:58:20 +0000

    test

---
+++ cancer_prediction/app.py
@@ -0,0 +1,21 @@
+import sys
+
+import typer
+from cancer_prediction import streamlit_app
+from streamlit.web import cli as stcli
+
+app = typer.Typer()
+
+@app.command()
+def __version__():
+    # Print the version of the app
+    typer.echo("0.1.0")
+
+@app.command()
+def run():
+    sys.argv = ["streamlit", "run", "cancer_prediction/streamlit_app.py"]
+    sys.exit(stcli.main())
+
+
+if __name__ == "__main__":
+    app()
---
+++ tests/test_CancerModel.py
@@ -0,0 +1,29 @@
+import unittest
+
+from cancer_prediction.cancer_model import CancerModel
+
+
+class TestCancerModel(unittest.TestCase):
+
+    def test_diagnosis_to_target(self):
+        model = CancerModel()
+        diagnosis = 'Malignant'
+        target = model.diagnosis_to_target(diagnosis)
+        self.assertEqual(target, 0)
+
+        diagnosis = 'Benign'
+        target = model.diagnosis_to_target(diagnosis)
+        self.assertEqual(target, 1)
+
+    def test_target_to_diagnosis(self):
+        model = CancerModel()
+        target = 0
+        diagnosis = model.target_to_diagnosis(target)
+        self.assertEqual(diagnosis, 'Malignant')
+
+        target = 1
+        diagnosis = model.target_to_diagnosis(target)
+        self.assertEqual(diagnosis, 'Benign')
+
+if __name__ == '__main__':
+    unittest.main()
